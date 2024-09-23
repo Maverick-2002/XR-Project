d532022696034bce2fb23c94e3f03bcb6c98d494
Gauraang Goel <ggauraang@gmail.com>
2024-09-23 15:32:48 +0530

    Update CannonSetup.js

--- CannonSetup.js
+++ CannonSetup.js
@@ -1,12 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as CANNON from 'cannon-es';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { Movement } from './Movement.js';
 import { AssetSpawner } from './AssetSpawner.js';
 import { CameraCoordinates } from './CameraCoordinates.js';
 import { MouseCoordinates } from './MouseCoordinates.js';
-import { RoomBoundary } from './RoomBoundary.js'; // Adjust the path as needed
+import { RoomBoundary } from './RoomBoundary.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
